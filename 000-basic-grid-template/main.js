f0b7f20735ce86d65b5a7ea85575bae691f46e8d
Shuheb Ahmed <shuhebahmed@Shuhebs-MacBook-Pro.local>
2019-06-20 09:27:43 +0100

    basic grid

---
+++ 000-basic-grid-template/main.js
@@ -0,0 +1,34 @@
+var columnDefs = [
+    { field: "athlete", width: 150 },
+    { field: "age", width: 90 },
+    { field: "country", width: 120 },
+    { field: "year", width: 90 },
+    { field: "date", width: 110 },
+    { field: "sport", width: 110 },
+    { field: "gold", width: 100 },
+    { field: "silver", width: 100 },
+    { field: "bronze", width: 100 },
+    { field: "total", width: 100 }
+];
+
+var gridOptions = {
+    columnDefs: columnDefs,
+    rowData: null
+};
+
+// setup the grid after the page has finished loading
+document.addEventListener('DOMContentLoaded', function () {
+    var gridDiv = document.querySelector('#myGrid');
+    new agGrid.Grid(gridDiv, gridOptions);
+
+    // remove following lines and create rowData array to add own data
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.open('GET', 'https://raw.githubusercontent.com/ag-grid/ag-grid/master/packages/ag-grid-docs/src/olympicWinnersSmall.json');
+    httpRequest.send();
+    httpRequest.onreadystatechange = function () {
+        if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+            var httpResult = JSON.parse(httpRequest.responseText);
+            gridOptions.api.setRowData(httpResult.splice(0, 10));
+        }
+    };
+});
