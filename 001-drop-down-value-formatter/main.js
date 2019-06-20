5208c7c0f69ccadefa67d1624441f86de164b001
Shuheb Ahmed <shuhebahmed@Shuhebs-MacBook-Pro.local>
2019-06-20 10:02:50 +0100

    added translations

--- 001-drop-down-value-formatter/main.js
+++ 001-drop-down-value-formatter/main.js
@@ -1,19 +1,46 @@
-var columnDefs = [
-    {
-        headerName: 'Number',
-        field: 'val',
-        valueFormatter: getNumberCellFormatter()
+let translations = {
+    'de-DE': {
+        autosizeThiscolumn: 'autosize this DE',
+        autosizeAllColumns: 'autosize all DE',
+        _columnHeaderName_Number: 'Number DE',
+        _columnHeaderName_Currency: 'Currency DE',
+        _columnHeaderName_Date: 'Date DE'
     },
-    {
-        headerName: 'Currency',
-        field: 'val',
-        valueFormatter: getCurrencyValueFormatter()
+    'ja-JP': {
+        autosizeThiscolumn: 'autosize this JP',
+        autosizeAllColumns: 'autosize all JP',
+        _columnHeaderName_Number: 'Number JP',
+        _columnHeaderName_Currency: 'Currency JP',
+        _columnHeaderName_Date: 'Date JP'
     },
-    {
-        headerName: 'Date',
-        field: 'date',
-        valueFormatter: dateValueFormatter
+};
+
+function translate(key, currLocale, defaultValue) {
+    if (translations[currLocale] && translations[currLocale][key]) {
+        return translations[currLocale][key];
     }
+    return defaultValue;
+}
+
+var columnDefs = (reportingCountry) => [
+    {
+        headerName: translate('_columnHeaderName_Number', reportingCountry, 'Number'),
+        field: 'val',
+        valueFormatter: getNumberCellFormatter(),
+        colId: 'val-number',
+    },
+    {
+        headerName: translate('_columnHeaderName_Currency', reportingCountry, 'Currency'),
+        field: 'val',
+        valueFormatter: getCurrencyValueFormatter(),
+        colId: 'val-currency',
+    },
+    {
+        headerName: translate('_columnHeaderName_Date', reportingCountry, 'Date'),
+        field: 'date',
+        valueFormatter: dateValueFormatter,
+        colId: 'val-date',
+    },
 ];
 
 var rowData = [
@@ -22,52 +49,41 @@ var rowData = [
     { val: 4243.39, date: '11/10/2017' },
     { val: 14343, date: '10/09/2016' },
     { val: 345343434, date: '09/08/2015' },
-    { val: 98324324234322, date: '08/07/2014' }
+    { val: 98324324234322, date: '08/07/2014' },
 ];
 
 var gridOptions = {
     context: {
-        reportingCountry: 'gb-GB'
+        reportingCountry: 'gb-GB',
     },
-    columnDefs: columnDefs,
+    columnDefs: columnDefs('gb-GB'),
     rowData: rowData,
     localeTextFunc: (key, currValue) => {
         let currLocale = gridOptions.context.reportingCountry;
-        if (translations[currLocale] && translations[currLocale][key]) return translations[currLocale][key];
-        return currValue;
-    }
-};
-
-let translations = {
-    'de-DE': {
-        autosizeThiscolumn: 'autosize this DE',
-        autosizeAllColumns: 'autosize all DE'
+        return translate(key, currLocale, currValue);
     },
-    'ja-JP': {
-        autosizeThiscolumn: 'autosize this JP',
-        autosizeAllColumns: 'autosize all JP'
-    }
+    deltaColumnMode: true,
 };
 
 function getCurrencyValueFormatter() {
     var gbpFormatter = new Intl.NumberFormat('gb-GB', {
         style: 'currency',
-        currency: 'GBP'
+        currency: 'GBP',
     });
     var eurFormatter = new Intl.NumberFormat('de-DE', {
         style: 'currency',
-        currency: 'EUR'
+        currency: 'EUR',
     });
     var usdFormatter = new Intl.NumberFormat('en-US', {
         style: 'currency',
-        currency: 'USD'
+        currency: 'USD',
     });
     var jpyFormatter = new Intl.NumberFormat('ja-JP', {
         style: 'currency',
-        currency: 'JPY'
+        currency: 'JPY',
     });
 
-    return function(params) {
+    return function (params) {
         switch (params.context.reportingCountry) {
             case 'de-DE':
                 return eurFormatter.format(params.value);
@@ -104,20 +120,24 @@ function getNumberCellFormatter() {
 function dateValueFormatter(params) {
     var dateAsString = params.value;
     var dateParts = dateAsString.split('/');
-    var cellDate = new Date(Number(dateParts[2]), Number(dateParts[1]) - 1, Number(dateParts[0]));
+    var cellDate = new Date(
+        Number(dateParts[2]),
+        Number(dateParts[1]) - 1,
+        Number(dateParts[0])
+    );
     var country = params.context.reportingCountry;
     return cellDate.toLocaleDateString(country);
 }
 
 function countryChanged() {
-    var value = document.getElementById('country').value;
-    gridOptions.context = { reportingCountry: value };
+    let reportingCountry = document.getElementById('country').value;
+    gridOptions.context = { reportingCountry: reportingCountry };
+    gridOptions.api.setColumnDefs(columnDefs(reportingCountry))
     gridOptions.api.refreshCells({ force: true });
-    gridOptions.api.refreshHeader();
 }
 
 // setup the grid after the page has finished loading
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var gridDiv = document.querySelector('#myGrid');
     new agGrid.Grid(gridDiv, gridOptions);
 });
